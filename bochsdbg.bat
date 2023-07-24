9a5bd101a915cd128501572a0475afe58598c9cc
Dreg <dreg@fr33project.org>
2023-07-24 16:19:33 +0200

    first

---
+++ bochsdbg.bat
@@ -0,0 +1,12 @@
+:loopez
+echo by David Reguera Garcia aka Dreg dreg@fr33project.org
+echo https://github.com/therealdreg/ https://www.fr33project.org @therealdreg
+cd %~dp0
+del /F /Q *.lock
+del /F /Q bx_enh_dbg.ini
+mkdir sharedfolder
+bochs.exe
+del /F /Q *.lock
+del /F /Q bx_enh_dbg.ini
+echo press enter to re execute
+jmp loopez
---
+++ bxversion.h
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////
+// This file is checked in as bxversion.h.in.  The configure script
+// substitutes variables and creates bxversion.h.
+/////////////////////////////////////////////////////////////////////////
+
+#define VERSION       "2.7.dreg"
+#define VER_SVNFLAG   1
+#define REL_STRING    "Built by Dreg https://github.com/therealdreg dreg@fr33project.org"
+#define REL_TIMESTAMP "2023-07-24"
